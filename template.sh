ceff3408b716acd78e1f710c529e93b12cddc097
Dave Moten <davidmoten@gmail.com>
2014-05-07 15:49:28 +1000

    use bash functions

--- template.sh
+++ template.sh
@@ -1,10 +1,31 @@
 #!/bin/bash
+## stop the script if an error occurs
 set -e
-TEMP_DIRECTORY=/tmp
-TEMPFILE=`mktemp $TEMP_DIRECTORY/ScriptXXXXX|sed -e 's/\.//g'`
-CLASS_NAME=`basename "$TEMPFILE"`
-JAVA_SOURCE=$TEMPFILE.java
+
+function prepare {
+  TEMP_DIRECTORY=/tmp
+  TEMPFILE=`mktemp $TEMP_DIRECTORY/ScriptXXXXX|sed -e 's/\.//g'`
+  CLASS_NAME=`basename "$TEMPFILE"`
+  JAVA_SOURCE=$TEMPFILE.java
+}
+
+function compile {
+  ## change the name of the class to match the file
+  sed "s/public class Script /public class $CLASS_NAME /g" $JAVA_SOURCE.tmp >$JAVA_SOURCE
+ 
+  ## compile the java
+  javac $JAVA_SOURCE 
+}
+
+function run {
+  ## run the class using all passed in parameters from the command line
+  java -classpath $TEMP_DIRECTORY $CLASS_NAME
+}
+
+prepare
+
 cat <<EOF >$JAVA_SOURCE.tmp
+////////////////////////////////////////////////////////////
 
 //Write your java class here with a main method
 //Be sure to leave the name of the class as Script 
@@ -22,14 +43,11 @@ public class Script {
 
 }
 
+///////////////////////////////////////////////////////////
 EOF
 
-## change the name of the class to match the file
-sed "s/public class Script /public class $CLASS_NAME /g" $JAVA_SOURCE.tmp >$JAVA_SOURCE
+compile
+run
 
-## compile the java
-javac $JAVA_SOURCE 
 
-## run the class using all passed in parameters from the command line
-java -classpath $TEMP_DIRECTORY $CLASS_NAME
 
