c10f7692be6ab6d2cefe83419ea801d9b468179f
Dave Moten <davidmoten@gmail.com>
2014-05-07 15:18:13 +1000

    update README.md

--- README.md
+++ README.md
@@ -6,3 +6,19 @@ then groovy is very close and a good option but there is the alternative to writ
  like [template.sh](https://github.com/davidmoten/java-script-template/blob/master/template.sh).
 
 The script is designed for linux with a bash shell.
+
+The example has this code embedded in it:
+
+```java
+public class Script {
+
+    public static void main(String[] args) throws Exception {
+       System.out.println("Here's a test run");
+    }
+
+}
+```
+
+The script is run by entering ./template.sh at the command line.
+
+On my i7 desktop it takes 2 seconds to run (compile and execute).
--- template.sh
+++ template.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
-TEMPFILE=`mktemp /tmp/ScriptXXXXX|sed -e 's/\.//g'`
+TEMP_DIRECTORY=/tmp
+TEMPFILE=`mktemp $TEMP_DIRECTORY/ScriptXXXXX|sed -e 's/\.//g'`
 CLASS_NAME=`basename "$TEMPFILE"`
 JAVA_SOURCE=$TEMPFILE.java
 cat <<EOF >$JAVA_SOURCE.tmp
@@ -16,7 +17,7 @@ import java.util.*;
 public class Script {
 
     public static void main(String[] args) throws Exception {
-       System.out.println("Here's a test run");
+       System.out.println("Here's a test run for you");
     }
 
 }
@@ -30,5 +31,5 @@ sed "s/public class Script /public class $CLASS_NAME /g" $JAVA_SOURCE.tmp >$JAVA
 javac $JAVA_SOURCE 
 
 ## run the class using all passed in parameters from the command line
-java -classpath /tmp $CLASS_NAME
+java -classpath $TEMP_DIRECTORY $CLASS_NAME
 
