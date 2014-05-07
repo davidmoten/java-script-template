57d61f079c42f5edcbd4b5eae64b5435fe6fa56f
Dave Moten <davidmoten@gmail.com>
2014-05-08 08:03:01 +1000

    add runjava with example

---
+++ Example.java
@@ -0,0 +1,15 @@
+//Write your java class here with a main method
+//Be sure to leave the name of the class as Script 
+
+//import some utilities
+import java.io.*;
+import java.text.*;
+import java.util.*;
+
+public class Script {
+
+    public static void main(String[] args) throws Exception {
+       System.out.println("Hello world!");
+    }
+
+}
