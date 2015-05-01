3afb0159261ed7df88484b184b384a93edff8497
Dave Moten <davidmoten@gmail.com>
2015-05-01 16:12:45 +1000

    Update Example.java

--- Example.java
+++ Example.java
@@ -1,12 +1,11 @@
 //Write your java class here with a main method
-//Be sure to leave the name of the class as Script 
 
 //import some utilities
 import java.io.*;
 import java.text.*;
 import java.util.*;
 
-public class Script {
+public class Example {
 
     public static void main(String[] args) throws Exception {
        System.out.println("Hello world! argsCount="+args.length);
